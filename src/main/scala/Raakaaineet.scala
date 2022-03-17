9a577b7d7d3d8766f641db4b029350450b67bf01
user1 <mohammad.hakimi@aalto.fi>
2022-03-17 12:42:14 +0200

    I started coding!

---
+++ src/main/scala/Raakaaineet.scala
@@ -0,0 +1,22 @@
+class Raakaaineet (määrä: Int){
+
+
+
+
+
+
+  var tonnikala=  3
+  var tomaatti =  4
+  var maito    =  3
+  var leipä    =  3
+  var kanamuna =  3
+  var taikina  =  4
+  var juusto   =  4
+  var pesto    =  5
+  var olioljy  =  4
+
+
+
+
+
+}
